c5cc602e7263d61200cf4f5ac8e41b27efcab9e0
github username <github email address>
2017-09-20 14:14:49 -0400

    built out first test reducer, connected provider, created store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import questionsReducer from './reducers/questionsReducer';
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(questionsReducer)
+console.log(store)
+
+ReactDOM.(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/reducers/questionsReducer.js
@@ -0,0 +1,5 @@
+function questionsReducer(state= {}, action) {
+  return state
+}
+
+export default questionsReducer
